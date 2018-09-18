cdd0e4b7338045b2fb9fc67d46942742520a1721
尹玉麒 <1162898171@qq.com>
2018-09-18 16:42:25 +0800

    Initial commit

---
+++ src/main/java/com/example/springtest/SpringtestApplication.java
@@ -0,0 +1,16 @@
+package com.example.springtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+
+@SpringBootApplication
+public class SpringtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringtestApplication.class, args);
+
+
+    }
+}
