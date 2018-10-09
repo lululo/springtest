d32d888b751d4b6a0aafd0f4b64f0b3384af409c
尹玉麒 <1162898171@qq.com>
2018-10-09 08:57:09 +0800

    redis

--- src/main/java/com/example/springtest/web/helloworld.java
+++ src/main/java/com/example/springtest/web/helloworld.java
@@ -2,10 +2,12 @@ package com.example.springtest.web;
 
 import com.example.springtest.common.RedisUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class helloworld {
     @Autowired
     RedisUtil redisUtil;
@@ -13,5 +15,10 @@ public class helloworld {
     public void show(){
         redisUtil.set("h","hkad");
         System.out.println(redisUtil);
+    }
+    @ResponseBody
+    @RequestMapping("show")
+    public String showString(){
+        return "hello world";
     }
 }
